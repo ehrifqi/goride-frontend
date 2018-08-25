de0fec241fcbda9660d34ef4e047379ce94df1c6
Fernando Christyanto <christyantofernando@gmail.com>
2018-08-25 21:38:12 +0700

    Add CustomerLogin route

--- src/containers/AppRoute.js
+++ src/containers/AppRoute.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import Home from './Home';
 import Login from './Login';
+import CustomerLogin from './CustomerLogin';
 
 const AppRoute = props => {
   return (
@@ -12,6 +13,7 @@ const AppRoute = props => {
           <Switch>
               <Route exact path="/" render={props => <Home {...props} />} />
               <Route exact path="/login" render={props => <Login {...props} />} />
+              <Route exact path="/login/customer" render={props => <CustomerLogin {...props} />} />
           </Switch>
       </div>
   )
