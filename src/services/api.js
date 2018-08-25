4dc4e309aabb5c5a70120f3ed01ba8272ea237ad
Fernando Christyanto <christyantofernando@gmail.com>
2018-08-26 03:22:56 +0700

    Sets BASE_URL in services/api

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,25 @@
 import axios from 'axios';
 
-export function setTokenHeader(token){
-    if(token){
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    }
-    else{
-        delete axios.defaults.headers.common['Authorization'];
-    }
+export function setTokenHeader(token) {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  }
+  else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
 }
-export function apiCall(method, path, data){
-    return new Promise((resolve, reject) => {
-        return axios[method](path, data).then(res => {
-            if(res.status === 200)
-                resolve(res.data);
-            else
-                reject(res.data);
-        })
-        .catch(err => {
-            reject(err.response.data.error);
-        })
+export function apiCall(method, path, data) {
+  const BASE_URL = 'https://bncc-goride-api.herokuapp.com/api/v1';
+  return new Promise((resolve, reject) => {
+    return axios[method](BASE_URL + path, data).then(res => {
+      if (res.status >= 200 && res.status < 300)
+        resolve(res.data);
+      else {
+        reject(res);
+      }
     })
+      .catch(err => {
+        reject(err.response);
+      })
+  })
 }
