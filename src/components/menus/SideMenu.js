80d7162dee8219cce6552713eb1b9b13c854709a
Fernando Christyanto <christyantofernando@gmail.com>
2018-09-01 18:40:53 +0700

    Add sidemenu component

---
+++ src/components/menus/SideMenu.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import './SideMenu.css';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class SideMenu extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  getActiveClass = id => {
+    if (this.props.activeId === id) {
+      return "active"
+    }
+    return " "
+  }
+
+  mapCustomerMenusToSideMenu = () => {
+    return this.props.customerMenus.map((menu) => {
+      return (
+        <a
+          key={menu.id}
+          className={`item ${this.getActiveClass(menu.id)}`}
+          id={menu.id}
+          onClick={() => this.props.setActiveId(menu.id, menu.redirectTo)}>
+          {menu.displayName}
+        </a>
+      )
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="sidemenu" style={{ minHeight: '90vh' }}>
+        {this.mapCustomerMenusToSideMenu()}
+      </div>
+    )
+  }
+}
+
+SideMenu.propTypes = {
+  customerMenus: PropTypes.array.isRequired,
+  activeId: PropTypes.string.isRequired,
+  setActiveId: PropTypes.func.isRequired
+}
+
+export default withRouter(SideMenu)
