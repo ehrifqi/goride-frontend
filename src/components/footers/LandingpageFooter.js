1d9768aa18dedbdfaf8d109392ab974973f75494
Fernando Christyanto <christyantofernando@gmail.com>
2018-09-01 18:35:03 +0700

    Modify LandingpageFooter to accept isAbsolute props

--- src/components/footers/LandingpageFooter.js
+++ src/components/footers/LandingpageFooter.js
@@ -3,10 +3,12 @@ import {withRouter} from 'react-router-dom'
 
 import './LandingpageFooter.css';
 import gojekLogo from './../../assets/img/gojek-logo.png';
+import PropTypes from 'prop-types';
 
 const LandingpageFooter = props => {
+  const absoluteFooterClass = (props.isAbsolute !== undefined && props.isAbsolute === false) ? "" : "footer--absolute";
   return (
-    <footer className="footer">
+    <footer className={`footer ${absoluteFooterClass}`}>
       <div className="footer__title">
         <span>Powered by </span>
         <img src={gojekLogo} alt="goride logo" className="footer__img" />
@@ -14,6 +16,10 @@ const LandingpageFooter = props => {
       <p>Copyright 2018</p>
     </footer>
   )
+}
+
+LandingpageFooter.propTypes = {
+  isAbsolute: PropTypes.bool
 }
 
 export default LandingpageFooter
