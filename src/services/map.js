00235f6d38df824e952ef4e19fb92fe188746bc6
Fernando Christyanto <christyantofernando@gmail.com>
2018-09-02 06:27:44 +0700

    Add services/map.js getDistanceInMeter

--- src/services/map.js
+++ src/services/map.js
@@ -19,30 +19,50 @@ exports.trackLocation = (onSuccess, onError = () => { }) => {
 exports.clearLocationTrack = trackLocationId => { navigator.geolocation.clearWatch(trackLocationId) }
 
 exports.getMapOptions = (lat, lng, zoom = 17) => {
-  return { 
-    center: new google.maps.LatLng(lat, lng), 
-    zoom: zoom }
+  return {
+    center: new google.maps.LatLng(lat, lng),
+    zoom: zoom
+  }
 }
 
-exports.position = (lat, lng) => ({lat: lat, lng: lng})
+exports.position = (lat, lng) => ({ lat: lat, lng: lng })
 
-exports.createMarker = (lat, lng, map, icon=undefined, title="", cb=undefined) => {
+exports.createMarker = (lat, lng, map, icon = undefined, title = "", cb = undefined) => {
   const marker = new google.maps.Marker({
-    position: {lat, lng},
+    position: { lat, lng },
     icon: icon,
     map: map,
     title: title
   });
 
-  if(cb)
+  if (cb)
     cb(marker);
 }
 
-exports.clearMarkersFromMap = (markers, cb=undefined) => {
+exports.clearMarkersFromMap = (markers, cb = undefined) => {
   for (let i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
 
-  if(cb)
+  if (cb)
     cb();
+}
+
+exports.getDistanceInMeter = (srcLat, srcLng, dstLat, dstLng, avoidTolls, avoidHighways, cb) => {
+  const directionsService = new google.maps.DirectionsService();
+
+  directionsService.route({
+    origin: new google.maps.LatLng(srcLat, srcLng),
+    destination: new google.maps.LatLng(dstLat, dstLng),
+    travelMode: google.maps.TravelMode.DRIVING,
+    avoidHighways: avoidHighways,
+    avoidTolls: avoidTolls
+  }, (response, status) => {
+    if (status == google.maps.DirectionsStatus.OK) {
+      cb(response.routes[0].legs[0].distance.value);
+    }
+    else{
+      return undefined;
+    }
+  });
 }
