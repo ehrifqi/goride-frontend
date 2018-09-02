42716ec8d263e012ab0df835a2775afa174c92f8
Fernando Christyanto <christyantofernando@gmail.com>
2018-09-03 00:51:28 +0700

    Add RESET_TOKEN action & refactor setCurrentUser

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,82 +1,91 @@
-import { SET_CURRENT_USER } from '../actionTypes';
-import { apiCall, setTokenHeader } from '../../services/api';
-import { store } from '../index';
+import { SET_CURRENT_USER, RESET_TOKEN } from "../actionTypes";
+import { apiCall, setTokenHeader } from "../../services/api";
+import { store } from "../index";
 
 /**
  * ACTION CREATORS
  */
-export function setCurrentUser(user, isAuthenticated = false){
+export function setCurrentUser(user, token, isAuthenticated = false) {
   return {
     type: SET_CURRENT_USER,
     isAuthenticated,
+    token,
     user
-  }
+  };
 }
 
-export function signInMember(data){
+export function reSetToken(token, isAuthenticated = false) {
+  return {
+    type: RESET_TOKEN,
+    token,
+    isAuthenticated
+  };
+}
+
+export function signInMember(data) {
   return dispatch => {
     return new Promise((resolve, reject) => {
       // const member = store.getState()["currentUser"];
-      return apiCall('post', `/auth/members_auth/login`, data)
+      return apiCall("post", `/auth/members_auth/login`, data)
         .then(user => {
-          localStorage.setItem('token', user.token.access_token);
+          localStorage.setItem("token", user.token.access_token);
+          dispatch(setCurrentUser(user.member, user.token.access_token, true));
+          resolve(user);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  };
+}
+
+export function signInDriver(data) {
+  return dispatch => {
+    return new Promise((resolve, reject) => {
+      // const member = store.getState()["currentUser"];
+      return apiCall("post", `/auth/drivers_auth/login`, data)
+        .then(user => {
+          localStorage.setItem("token", user.token.access_token);
           dispatch(setCurrentUser(user, true));
           resolve(user);
         })
         .catch(err => {
           reject(err);
         });
-    })
-  }
+    });
+  };
 }
 
-export function signInDriver(data){
+export function registerMember(data) {
   return dispatch => {
     return new Promise((resolve, reject) => {
       // const member = store.getState()["currentUser"];
-      return apiCall('post', `/auth/drivers_auth/login`, data)
+      return apiCall("post", `/auth/members_auth/register`, data)
         .then(user => {
-          localStorage.setItem('token', user.token.access_token);
+          localStorage.setItem("token", user.token.access_token);
           dispatch(setCurrentUser(user, true));
           resolve(user);
         })
         .catch(err => {
           reject(err);
         });
-    })
-  }
+    });
+  };
 }
 
-export function registerMember(data){
+export function registerDriver(data) {
   return dispatch => {
     return new Promise((resolve, reject) => {
       // const member = store.getState()["currentUser"];
-      return apiCall('post', `/auth/members_auth/register`, data)
+      return apiCall("post", `/auth/drivers_auth/register`, data)
         .then(user => {
-          localStorage.setItem('token', user.token.access_token);
+          localStorage.setItem("token", user.token.access_token);
           dispatch(setCurrentUser(user, true));
           resolve(user);
         })
         .catch(err => {
           reject(err);
         });
-    })
-  }
-}
-
-export function registerDriver(data){
-  return dispatch => {
-    return new Promise((resolve, reject) => {
-      // const member = store.getState()["currentUser"];
-      return apiCall('post', `/auth/drivers_auth/register`, data)
-        .then(user => {
-          localStorage.setItem('token', user.token.access_token);
-          dispatch(setCurrentUser(user, true));
-          resolve(user);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    })
-  }
+    });
+  };
 }
--- src/store/reducers/currentUser.js
+++ src/store/reducers/currentUser.js
@@ -1,16 +1,25 @@
-import { SET_CURRENT_USER } from '../actionTypes';
+import { SET_CURRENT_USER, RESET_TOKEN } from '../actionTypes';
 
 const initialState = {
   isAuthenticated: false,
-  user: {}
+  user: {},
+  token: ''
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case SET_CURRENT_USER:
       return {
+        ...state,
         isAuthenticated: action.isAuthenticated,
-        user: action.user
+        user: action.user,
+        token: action.token
+      }
+    case RESET_TOKEN:
+      return {
+        ...state,
+        isAuthenticated: action.isAuthenticated,
+        token: action.token
       }
     default:
       return initialState;
